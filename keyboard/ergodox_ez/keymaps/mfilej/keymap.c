56c5b180c787aacbd1be2bef10f24770d89b2721
Miha Filej <miha@filej.net>
2016-04-04 13:50:09 +0200

    Remove F20 mapping as it does not work on OS X

--- keyboard/ergodox_ez/keymaps/mfilej/keymap.c
+++ keyboard/ergodox_ez/keymaps/mfilej/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        | F11  | F12  | F13  | F14  | F15  |      |           |      |  F16 | F17  | F18  | F19  | F20  |        |
+ * |        | F11  | F12  | F13  | F14  | F15  |      |           |      |  F16 | F17  | F18  | F19  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_TRNS,KC_TRNS,KC_VOLD,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_F16,  KC_F17, KC_F18,  KC_F19,  KC_F20,  KC_TRNS,
+       KC_TRNS, KC_F16,  KC_F17, KC_F18,  KC_F19,  KC_TRNS, KC_TRNS,
                 KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_MPRV,KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS,
