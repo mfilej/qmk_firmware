dce0cefb493cc4c0b3d18dab024a41c0ae0af361
Miha Filej <miha@filej.net>
2017-01-24 13:27:06 +0100

    Move around Backspace and Tab

--- keyboards/ergodox/ez/keymaps/mfilej/keymap.c
+++ keyboards/ergodox/ez/keymaps/mfilej/keymap.c
@@ -13,19 +13,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
- * |--------+------+------+------+------+------|LCtrl |           | Bspc |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|LCtrl |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Fn   |      |      | LAlt | LGUI |                                       |  Fn  |      |      |      |       |
+ *   | Fn   |      |      | LAlt | LGUI |                                       |  Up  | Down | Left |Right |       |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Bspc |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
- *                                 | Space| Tab  |------|       |------|  Esc   |Enter |
+ *                                 | Space| Bspc |------|       |------|  Esc   |Enter |
  *                                 |      |      |Hyper |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -34,18 +34,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_GRV,
-        KC_TRNS, KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
+        KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_LCTL,
         MO(FUNK),KC_TRNS, KC_TRNS,KC_LALT,KC_LGUI,
                                                   KC_BSPC,KC_TRNS,
                                                           KC_TRNS,
-                                          KC_SPC, KC_TAB, ALL_T(KC_NO),
+                                          KC_SPC, KC_BSPC,ALL_T(KC_NO),
         // right hand
         KC_BSLS  ,KC_6,     KC_7,   KC_8,    KC_9,     KC_0,    KC_EQL,
         KC_RBRC  ,KC_Y,     KC_U,   KC_I,    KC_O,     KC_P,    KC_MINS,
                   KC_H,     KC_J,   KC_K,    KC_L,     KC_SCLN, CTL_T(KC_QUOTE),
-        KC_BSPC  ,KC_N,     KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSHIFT,
+        KC_TRNS  ,KC_N,     KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSHIFT,
                             KC_DOWN,KC_UP,   KC_LEFT,  KC_RIGHT,KC_TRNS,
         KC_VOLU  ,KC_MPLY,
         KC_VOLD  ,
