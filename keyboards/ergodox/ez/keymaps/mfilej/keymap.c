6e9303c14877f3503884a0dcf3db9637916e99d1
Miha Filej <miha@filej.net>
2017-01-24 13:24:21 +0100

    Add arrow keys

--- keyboards/ergodox/ez/keymaps/mfilej/keymap.c
+++ keyboards/ergodox/ez/keymaps/mfilej/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RBRC  ,KC_Y,     KC_U,   KC_I,    KC_O,     KC_P,    KC_MINS,
                   KC_H,     KC_J,   KC_K,    KC_L,     KC_SCLN, CTL_T(KC_QUOTE),
         KC_BSPC  ,KC_N,     KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSHIFT,
-                            KC_ESC, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+                            KC_DOWN,KC_UP,   KC_LEFT,  KC_RIGHT,KC_TRNS,
         KC_VOLU  ,KC_MPLY,
         KC_VOLD  ,
         KC_TRNS  ,KC_ESC   ,KC_ENT
