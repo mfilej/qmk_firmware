7c7d9b57d875ae2340966e24a658a0e47f76418a
Miha Filej <miha@filej.net>
2017-01-26 09:22:25 +0100

    Move around backspace, tab, and esc

--- keyboards/ergodox/ez/keymaps/mfilej/keymap.c
+++ keyboards/ergodox/ez/keymaps/mfilej/keymap.c
@@ -22,10 +22,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   | Fn   |      |      | LAlt | LGUI |                                       |  Up  | Down | Left |Right |       |
  *   `----------------------------------'                                       `-----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | Bspc |      |       |      |        |
+ *                                        |      |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
- *                                 | Space| Bspc |------|       |------|  Esc   |Enter |
+ *                                 | Space|  Tab |------|       |------| Bspc   |Enter |
  *                                 |      |      |Hyper |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -38,18 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_LCTL,
         MO(FUNK),KC_TRNS, KC_TRNS,KC_LALT,KC_LGUI,
-                                                  KC_BSPC,KC_TRNS,
+                                                  KC_TRNS,KC_TRNS,
                                                           KC_TRNS,
-                                          KC_SPC, KC_BSPC,ALL_T(KC_NO),
+                                          KC_SPC, KC_TAB ,ALL_T(KC_NO),
         // right hand
         KC_BSLS  ,KC_6,     KC_7,   KC_8,    KC_9,     KC_0,    KC_EQL,
         KC_RBRC  ,KC_Y,     KC_U,   KC_I,    KC_O,     KC_P,    KC_MINS,
                   KC_H,     KC_J,   KC_K,    KC_L,     KC_SCLN, CTL_T(KC_QUOTE),
         KC_TRNS  ,KC_N,     KC_M,   KC_COMM, KC_DOT,   KC_SLSH, KC_RSHIFT,
                             KC_DOWN,KC_UP,   KC_LEFT,  KC_RIGHT,KC_TRNS,
-        KC_VOLU  ,KC_MPLY,
+        KC_VOLU  ,KC_TRNS,
         KC_VOLD  ,
-        KC_TRNS  ,KC_ESC   ,KC_ENT
+        KC_MPLY  ,KC_BSPC  ,KC_ENT
     ),
 /* Keymap 1: Function keys
  *
